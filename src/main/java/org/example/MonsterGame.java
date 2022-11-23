dae996e04509c6311911db7fff1da76805ea521d
Vytis Lasaitis <v.lasaitis@gmail.com>
2022-11-23 21:15:46 +0100

    Vytis refactoring 23:e

--- src/main/java/org/example/MonsterGame.java
+++ src/main/java/org/example/MonsterGame.java
@@ -5,6 +5,7 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class MonsterGame {
@@ -12,40 +13,49 @@ public class MonsterGame {
         // Goal: Generera en bana med flera mindre hinder. Slumpmässigt
         // OOP för monster
         // Refaktorera i metoder
+        // Bug: Monster kan spawna PÅ obstacle
+        // Bug: Monster kan deleta obstacle vertikalt
+        /*
+        Vytis gjort:
+        - Skapat moveObject metod, tog bort monstermovement repeterande kod
+        - Ändrade så att playerCharacter också är ett Position objekt. Variabel: player
+        - Ändrade namnet på hjärtat (spelarens avatar) till playerCharacter
+        - Alltså överallt där det var x och y är det nu player.x och player.y
+        - Några förkortningar: monsterPosition : monPos
+         */
+
 
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false);
 
-        int x = 5;
-        int y = 5;
-        final char player = '\u2661';
+
+        Position player = new Position(5,5);
+        final char playerCharacter = '\u2661';
         final char block = '\u2588';
         final char monster = '\u2620';
-        terminal.setCursorPosition(x, y);
-        terminal.putCharacter(player);
+        terminal.setCursorPosition(player.x, player.y);
+        terminal.putCharacter(playerCharacter);
 
-        // Create obsticles array
-        Position[] obsticles = new Position[50];
+        // Create obstacles array
+        Position[] obstacles = new Position[50];
         for(int i = 0;i<50;i++){
-            obsticles[i] = new Position(10+i, 10);
+            obstacles[i] = new Position(10+i, 10);
         }
 
-        // Use obsticles array to print to lanterna
-        for (Position p : obsticles) {
+        // Use obstacles array to print to lanterna
+        for (Position p : obstacles) {
             terminal.setCursorPosition(p.x, p.y);
             terminal.putCharacter(block);
         }
 
         Random r = new Random();
-        Position monsterPosition = new Position(r.nextInt(40,80), r.nextInt(24));
-        terminal.setCursorPosition(monsterPosition.x, monsterPosition.y);
+        Position monPos = new Position(r.nextInt(40,80), r.nextInt(24));
+        terminal.setCursorPosition(monPos.x, monPos.y);
         terminal.putCharacter(monster);
 
         terminal.flush();
 
-
-        // Task 12
         boolean continueReadingInput = true;
         while (continueReadingInput) {
 
@@ -55,12 +65,7 @@ public class MonsterGame {
                 keyStroke = terminal.pollInput();
             } while (keyStroke == null);
 
-
-            KeyType type = keyStroke.getKeyType();
             Character c = keyStroke.getCharacter(); // used Character instead of char because it might be null
-
-            System.out.println("keyStroke.getKeyType(): " + type
-                    + " keyStroke.getCharacter(): " + c);
 
             if (c == Character.valueOf('q')) {
                 continueReadingInput = false;
@@ -68,86 +73,64 @@ public class MonsterGame {
                 System.out.println("quit");
             }
 
-            int oldX = x; // save old position x
-            int oldY = y; // save old position y
+            int oldX = player.x; // save old position x
+            int oldY = player.y; // save old position y
             switch (keyStroke.getKeyType()) {
-                case ArrowDown -> y += 1;
-                case ArrowUp -> y -= 1;
-                case ArrowRight -> x += 1;
-                case ArrowLeft -> x -= 1;
+                case ArrowDown -> player.y += 1;
+                case ArrowUp -> player.y -= 1;
+                case ArrowRight -> player.x += 1;
+                case ArrowLeft -> player.x -= 1;
             }
+            // detect if playerCharacter tries to run into obsticle
 
-            // detect if player tries to run into obsticle
-            boolean crashIntoObsticle = false;
-            for (Position p : obsticles) {
-                if (p.x == x && p.y == y) {
-                    crashIntoObsticle = true;
-                    break;
-                }
-            }
+            boolean crashIntoObstacle = checkForObstacleCrash(obstacles, player.x, player.y);
 
-            if (crashIntoObsticle) {
-                x = oldX;
-                y = oldY;
+            if (crashIntoObstacle) {
+                player.x = oldX;
+                player.y = oldY;
             }
             else {
-                terminal.setCursorPosition(oldX, oldY); // move cursor to old position
-                terminal.putCharacter(' '); // clean up by printing space on old position
-                terminal.setCursorPosition(x, y);
-                terminal.putCharacter(player);
+                moveObject(oldX, oldY, player.x, player.y, playerCharacter, terminal);
             }
 
             // MONSTER MOVEMENT
-            int monsterOldX = monsterPosition.x;
-            int monsterOldY = monsterPosition.y;
+            int monOldX = monPos.x;
+            int monOldY = monPos.y;
+            //testade metod för att ta bort lines under men funkade inte..
+            //setMonsterCoordinates(monPos.x, monPos.y, player.x, player.y);
+            if (monPos.x > player.x) {
+                monPos.x--;
+            } else if (monPos.x < player.x){
+                monPos.x++;
+            }
+            if (monPos.y > player.y) {
+                monPos.y--;
+            } else if (monPos.y < player.y){
+                monPos.y++;
+            }
 
-            if (monsterPosition.x > x) {
-                monsterPosition.x--;
-            } else if (monsterPosition.x < x){
-                monsterPosition.x++;
-            }
-            if (monsterPosition.y > y) {
-                monsterPosition.y--;
-            } else if (monsterPosition.y < y){
-                monsterPosition.y++;
-            }
-
-            boolean monsterCrashIntoObsticle = false;
-            for (Position p : obsticles) {
-                if (p.x == monsterPosition.x && p.y == monsterPosition.y) {
-                    monsterCrashIntoObsticle = true;
-                    break;
-                }
-            }
+            boolean monsterCrash = checkForObstacleCrash(obstacles,monPos.x, monPos.y);
 
             // helps monster move around obstacle when hitting it
-            if (monsterCrashIntoObsticle) {
-                if (monsterPosition.x > x) {
-                    monsterPosition.x = monsterOldX-1;
-                    monsterPosition.y = monsterOldY;
-                } else if (monsterPosition.x < x) {
-                    monsterPosition.x = monsterOldX+1;
-                    monsterPosition.y = monsterOldY;
+            if (monsterCrash) {
+                if (monPos.x > player.x) {
+                    monPos.x = monOldX-1;
+                    monPos.y = monOldY;
+                } else if (monPos.x < player.x) {
+                    monPos.x = monOldX+1;
+                    monPos.y = monOldY;
                 } else {
-                    monsterPosition.x = monsterOldX;
-                    monsterPosition.y = monsterOldY;
+                    monPos.x = monOldX;
+                    monPos.y = monOldY;
                 }
-                //moveObject(int oldY, int oldX, int newX, int newY, char character, Terminal terminal);
-                terminal.setCursorPosition(monsterOldX, monsterOldY);
-                terminal.putCharacter(' ');
-                terminal.setCursorPosition(monsterPosition.x, monsterPosition.y);
-                terminal.putCharacter(monster);
-                terminal.flush();
+                moveObject(monOldX, monOldY, monPos.x, monPos.y, monster, terminal);
+
             } else {
-                terminal.setCursorPosition(monsterOldX, monsterOldY);
-                terminal.putCharacter(' ');
-                terminal.setCursorPosition(monsterPosition.x, monsterPosition.y);
-                terminal.putCharacter(monster);
-                terminal.flush();
+                moveObject(monOldX, monOldY, monPos.x, monPos.y, monster, terminal);
             }
 
-            // check if player runs into the monster
-            if (monsterPosition.x == x && monsterPosition.y == y) {
+            // check if playerCharacter runs into the monster
+            if (monPos.x == player.x && monPos.y == player.y) {
                 terminal.close();
                 continueReadingInput = false;
             }
@@ -155,4 +138,32 @@ public class MonsterGame {
             terminal.flush();
         }
     }
+    public static void moveObject(int oldX, int oldY, int newX, int newY, char character, Terminal terminal) throws IOException {
+        terminal.setCursorPosition(oldX, oldY);
+        terminal.putCharacter(' ');
+        terminal.setCursorPosition(newX, newY);
+        terminal.putCharacter(character);
+        terminal.flush();
+    }
+    public static boolean checkForObstacleCrash(Position[] obstacles, int currentX, int currentY) {
+        for (Position p : obstacles) {
+            if (p.x == currentX && p.y == currentY) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void setMonsterCoordinates(int monX, int monY, int plyrX, int plyrY) {
+        if (monX > plyrX) {
+            monX--;
+        } else if (monX < plyrX){
+            monX++;
+        }
+        if (monY > plyrY) {
+            monY--;
+        } else if (monY < plyrY){
+            monY++;
+        }
+    }
+
 }
